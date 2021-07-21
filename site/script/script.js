664428893bae26fd4189105bfe197b22d8948a7a
samuelpswang <samuelpswang@gmail.com>
2021-07-21 21:56:12 +0800

    Add buttons to index.html
    * Add buttons to index html
    * Format alignment in style.css
    * Test button functionality with basic JavaScript

---
+++ site/script/script.js
@@ -0,0 +1,39 @@
+function buttonOne() {
+    console.log("buttonOne()");
+}
+
+function buttonTwo() {
+    console.log("buttonTwo()");
+}
+
+function buttonThree() {
+    console.log("buttonThree()");
+}
+
+function buttonFour() {
+    console.log("buttonFour()");
+}
+
+function buttonFive() {
+    console.log("buttonFive()");
+}
+
+function buttonSix() {
+    console.log("buttonSix()");
+}
+
+function buttonSeven() {
+    console.log("buttonSeven()");
+}
+
+function buttonEight() {
+    console.log("buttonEight()");
+}
+
+function buttonNine() {
+    console.log("buttonNine()");
+}
+
+function buttonTen() {
+    console.log("buttonTen()");
+}
